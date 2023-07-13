c6f3ff9a6861afd914901b1e8fedc55cc18ab61f
norbertbla <blaszczyk.norbert@outlook.com>
2023-07-13 13:53:36 +0000

    Added site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://norbertbla.com',
   integrations: [
     react(),
     tailwind({
